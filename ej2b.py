7eedb2e17801c3409057e7ee065c3c41e8311f00
Nico37-coder <64267379+Nico37-coder@users.noreply.github.com>
2022-04-12 18:42:08 -0300

    Update ej2b.py
    12042022

--- ej2b.py
+++ ej2b.py
@@ -1,5 +1,5 @@
 # Si, existe una correlación lineal entre las columnas Salario Bruto y Neto. Se propone eliminar de la encuesta la columna de Salario Bruto para que la encuesta sea mas simple.
-# Se utiliza el coeficiente de Pearson para determinar la correlación. Esté tiene un valor de 0,83 lo que indica una alta correlación positiva entre las columnas.
+# Se utiliza el **coeficiente de Pearson** para determinar la correlación. Esté tiene un valor de **0,83** lo que indica una alta correlación positiva entre las columnas.
 
 #  Crear subconjunto de df.
 df_s = df[["salary_monthly_BRUTO","salary_monthly_NETO"]]
@@ -11,7 +11,8 @@ df_s = df_s.dropna(how='all')
 # Coeficiente de Pearson
 df_s["salary_monthly_BRUTO"].corr(df_s["salary_monthly_NETO"])
 
-import matplotlib.pyplot as plt
-
-plt.plot(df_s["salary_monthly_BRUTO"], df_s["salary_monthly_NETO"],"ro")
-plt.title("Correlación Salario Bruto vs Salario Neto")
+# Esta correlación positiva entre las dos variables se puede apreciar también por medio del siguiente grafico:
+max_age = 99
+seaborn.pairplot(data=df[df.profile_age < max_age], y_vars=['salary_monthly_BRUTO'],
+                 x_vars=['salary_monthly_NETO'],
+                 aspect=3, height=4, dropna= True)
